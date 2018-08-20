4c0fcfbf34793c8472a38f95d6162eaa35be44b1
estefancaique <estefancaique@gmail.com>
2018-08-20 00:04:42 -0300

    Terraform
    Arquivo de Deploy do servidor

--- terraform/caique.tf
+++ terraform/caique.tf
@@ -1,10 +1,41 @@
 provider "aws" {
   access_key = "*****"
-  secret_key = "******"
+  secret_key = "*****"
   region     = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+# Resource configuration
+resource "aws_instance" "idwall" {
   ami           = "ami-c80b0aa2"
   instance_type = "t2.micro"
 }
+
+
+resource "aws_security_group" "sg" {
+  name = "${var.name}-sg"
+  description = "Allow all inbound traffic"
+
+  ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+	  
+  }
+
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+      from_port = 443
+      to_port = 443
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
